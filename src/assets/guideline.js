f3f3eab0df1918845f2f0477cd3d0ae98383405c
Erick Gannem <erickgannem@gmail.com>
2021-02-22 17:41:40 -0400

    add guideline.js

---
+++ src/assets/guideline.js
@@ -0,0 +1,18 @@
+const colors = {
+  dark01: '#0A141C',
+  dark02: '#0E1B26',
+  dark03: '#222F3A',
+  light01: '#E2E2E2',
+  blueAccent: '#00A3FF',
+  orangeAccent: '#FF9900',
+  greenAccent: '#44C23A',
+  yellowAccent: '#FFA031',
+  brownAccent: '#311E07',
+  logoAccent: '#00A266'
+}
+
+const layout = {
+  padding15: 15
+}
+
+export { colors, layout }
