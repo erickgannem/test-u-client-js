3ef69c487bdf277cb4eb7e6cdddde07f1fb51d0f
Erick Gannem <erickgannem@gmail.com>
2021-03-08 17:52:16 -0400

    add google maps

--- src/views/Schools/index.jsx
+++ src/views/Schools/index.jsx
@@ -1,13 +1,53 @@
 import React from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
 
 import { View } from '../elements'
 
+const containerStyle = {
+  width: '100%',
+  height: '100vh'
+}
 
-export default function Schools () {
+const center = {
+  lat: -3.745,
+  lng: -38.523
+}
+
+function Schools () {
+  const [, setMap] = React.useState(null)
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+  })
+
+  const onLoad = React.useCallback(function callback (map) {
+    const bounds = new window.google.maps.LatLngBounds()
+    map.fitBounds(bounds)
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback (map) {
+    setMap(null)
+  }, [])
+
   return (
     <View>
-      <h1>Schools Section</h1>
+      {
+        isLoaded
+          ? (
+              <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={20}
+                onLoad={onLoad}
+                onUnmount={onUnmount}
+              />
+            )
+          : (<></>)
+      }
     </View>
   )
 }
 
+export default React.memo(Schools)
